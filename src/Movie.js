933f37fea84b6dcc6f9d2c7ebe01d7b06a7357b8
Uni <joseoyun12@gmail.com>
2022-09-27 23:00:14 +0900

    Movie 컴포넌트  props,propTypes 추가

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie({ id, title, year, summary, poster }) {
+	return <h4>{title}</h4>;
+}
+
+Movie.propTypes = {
+	id: PropTypes.number.isRequired,
+	year: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	summary: PropTypes.string.isRequired,
+	poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
