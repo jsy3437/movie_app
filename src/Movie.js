2857a8e9f0efd7d276e47228dee43cb5cd83857a
Uni <joseoyun12@gmail.com>
2022-09-29 13:00:40 +0900

    해당 컴포넌트 최상단 div className 추가

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import './Movie.css';
 
 function Movie({ title, year, summary, poster, genres }) {
 	return (
-		<div>
+		<div className="movie">
 			<img src={poster} alt={title} title={title}></img>
 			<div className="movie__data">
 				<h3 className="movie__title" style={{ backgroundColor: 'red' }}>
