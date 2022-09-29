98252a91f5decd97090684dce1b90058a352fa16
Uni <joseoyun12@gmail.com>
2022-09-29 13:09:32 +0900

    영화 요약내용 글자 수 제한함

--- src/Movie.js
+++ src/Movie.js
@@ -20,7 +20,7 @@ function Movie({ title, year, summary, poster, genres }) {
 						);
 					})}
 				</ul>
-				<p className="movie__summary">{summary}</p>
+				<p className="movie__summary">{summary.slice(0, 180)}...</p>
 			</div>
 		</div>
 	);
