1cf238a68da65ed33196dbd577ca89acb6ed9225
Uni <joseoyun12@gmail.com>
2022-09-30 22:26:56 +0900

    추가화면과 페이지 전환을 위해 코드를 이동하고 router를 적용함

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './components/Movie';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
+import Home from './routes/Home';
+import About from './routes/About';
 
-class App extends React.Component {
-	state = {
-		isLoding: true,
-		movies: [],
-	};
-
-	getMovies = async () => {
-		const {
-			data: {
-				data: { movies },
-			},
-		} = await axios.get(
-			'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
-		);
-		this.setState({ movies, isLoding: false });
-		console.log(movies);
-	};
-	componentDidMount() {
-		// 영화 데이터 로딩!1
-		this.getMovies();
-	}
-	render() {
-		const { isLoding, movies } = this.state;
-		return (
-			<section className="container">
-				{isLoding ? (
-					<div className="loader">
-						<span className="loader__text">Loading...</span>
-					</div>
-				) : (
-					<div className="movies">
-						{movies.map((movie) => {
-							console.log(movie);
-							return (
-								<Movie
-									key={movie.id}
-									id={movie.id}
-									year={movie.year}
-									title={movie.title}
-									summary={movie.summary}
-									poster={movie.medium_cover_image}
-									genres={movie.genres}
-								/>
-							);
-						})}
-					</div>
-				)}
-			</section>
-		);
-	}
+function App() {
+	return (
+		<HashRouter>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/about" component={About} />
+		</HashRouter>
+	);
 }
 
 export default App;
