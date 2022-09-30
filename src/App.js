ef3cde2be2624888e0364b885337d240086ded50
Uni <joseoyun12@gmail.com>
2022-09-30 23:04:50 +0900

    페이지 이동을 위한 네비게이션 컴포넌트를 추가함

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App() {
 	return (
 		<HashRouter>
+			<Navigation />
 			<Route exact path="/" component={Home} />
 			<Route exact path="/about" component={About} />
 		</HashRouter>
