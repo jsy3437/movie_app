c38757203b34e58d5d178dde093de85ce662633a
Uni <joseoyun12@gmail.com>
2022-10-03 17:41:39 +0900

    영화 카드를 클릭 시 디테일 페이지로 이동하고, 해당 정보 전달하는 코드 추가함

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
 function Movie({ title, year, summary, poster, genres }) {
 	return (
 		<div className="movie">
-			<img src={poster} alt={title} title={title}></img>
-			<div className="movie__data">
-				<h3 className="movie__title" style={{ backgroundColor: 'red' }}>
-					{title}
-				</h3>
-				<h5 className="movie__year">{year}</h5>
-				<ul className="movie__genres">
-					{genres.map((genre, index) => {
-						return (
-							<li key={index} className="movie__genre">
-								{genre}
-							</li>
-						);
-					})}
-				</ul>
-				<p className="movie__summary">{summary.slice(0, 180)}...</p>
-			</div>
+			<Link
+				to={{
+					pathname: '/movie-detail',
+					state: { year, title, summary, poster, genres },
+				}}
+			>
+				<img src={poster} alt={title} title={title}></img>
+				<div className="movie__data">
+					<h3 className="movie__title" style={{ backgroundColor: 'red' }}>
+						{title}
+					</h3>
+					<h5 className="movie__year">{year}</h5>
+					<ul className="movie__genres">
+						{genres.map((genre, index) => {
+							return (
+								<li key={index} className="movie__genre">
+									{genre}
+								</li>
+							);
+						})}
+					</ul>
+					<p className="movie__summary">{summary.slice(0, 180)}...</p>
+				</div>
+			</Link>
 		</div>
 	);
 }
