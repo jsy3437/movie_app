86c9f8921bc2ed9f72eca65f4e378dabc22a8fa2
Uni <joseoyun12@gmail.com>
2022-10-03 17:45:54 +0900

    경로로 강제 접근시 Home으로 보내주고, 넘어온 영화 정보가 있을때만 화면 그리게 수정함

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Detail() {
-	return <div>디테일</div>;
+class Detail extends React.Component {
+	componentDidMount() {
+		const { location, history } = this.props;
+		if (location.state === undefined) {
+			history.push('/');
+		}
+	}
+
+	render() {
+		const { location } = this.props;
+		if (location.state) {
+			return <span>{location.state.title}</span>;
+		} else {
+			return null;
+		}
+	}
 }
 
 export default Detail;
