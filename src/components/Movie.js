a96f51f3fdd2c5d307c97b0bb3253c97bcbb54a3
Uni <joseoyun12@gmail.com>
2022-10-19 22:03:51 +0900

    디자인 변경으로 필요없는 요소 삭제

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,22 +13,6 @@ function Movie({ title, year, summary, poster, genres }) {
 				}}
 			>
 				<img src={poster} alt={title} title={title}></img>
-				<div className="movie__data">
-					<h3 className="movie__title" style={{ backgroundColor: 'red' }}>
-						{title}
-					</h3>
-					<h5 className="movie__year">{year}</h5>
-					<ul className="movie__genres">
-						{genres.map((genre, index) => {
-							return (
-								<li key={index} className="movie__genre">
-									{genre}
-								</li>
-							);
-						})}
-					</ul>
-					<p className="movie__summary">{summary.slice(0, 180)}...</p>
-				</div>
 			</Link>
 		</div>
 	);
