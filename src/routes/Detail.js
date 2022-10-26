a2244f3cee1cade7cf082757fed7a8faf4ce5f5b
Uni <joseoyun12@gmail.com>
2022-10-26 16:49:09 +0900

    디테일 데이터들 띄우는 코드를 추가함

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component {
 	componentDidMount() {
@@ -10,8 +11,23 @@ class Detail extends React.Component {
 
 	render() {
 		const { location } = this.props;
+
 		if (location.state) {
-			return <span>{location.state.title}</span>;
+			return (
+				<section className="container">
+					<div className="detail__warp">
+						<div className="detail__left">
+							<img src={location.state.poster} />
+						</div>
+						<div className="detail__right">
+							<h2>{location.state.title}</h2>
+							<p>{location.state.year}</p>
+							<p>{location.state.genres.join(', ')}</p>
+							<p>{location.state.summary}</p>
+						</div>
+					</div>
+				</section>
+			);
 		} else {
 			return null;
 		}
