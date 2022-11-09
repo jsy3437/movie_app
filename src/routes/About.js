61211b66ab26570753acbc6b62d3d1eb5fed996f
Uni <joseoyun12@gmail.com>
2022-11-10 00:25:42 +0900

    소개 할 정보 입력함

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './About.css';
+import doll from '../images/doll.jpg';
+import githubIcon from '../images/ico_github.svg';
+import emailIcon from '../images/ico_email.svg';
 
 function About() {
 	return (
 		<div className="about__container">
-			<span>
-				"Freedom is the freedom to say that two plus two make four. If that is
-				granted, all else follows."
-			</span>
-			<span>- George Orwell, 1984</span>
+			<div className="doll__box">
+				<img src={doll} alt="doll image" />
+			</div>
+			<div className="text__box">
+				<h2>Seoyoon</h2>
+				<p>Web Front-end Developer</p>
+				<h5>React, JavaScript, CSS, HTML</h5>
+				<p>
+					<img src={emailIcon} alt="email icon" />
+					joseoyoon12@gmail.com
+				</p>
+				<p>
+					<img src={githubIcon} alt="github icon" />
+					jsy3437
+				</p>
+			</div>
 		</div>
 	);
 }
